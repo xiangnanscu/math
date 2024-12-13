41445f9aebbbcda99edd2517e2674d989edfcbf3
xiangnan <280145668@qq.com>
2024-12-13 22:52:45 +0800

    黑色方块

--- split.mjs
+++ split.mjs
@@ -53,6 +53,9 @@ function parseHeader(line) {
 const makeMathJaxCompatible = (content) => {
   return content
   // .replace(/^## (\d+)\./g, '$1. ')
+  .replace(/(^[I])\s([\u4e00-\u9fa5])/g, (match) => {
+    return `$\blacksquare$`
+  })
   .replace(/^([a-z]\)\s+\S+)/g, (match) => {
     return `**${match}**`
   })
